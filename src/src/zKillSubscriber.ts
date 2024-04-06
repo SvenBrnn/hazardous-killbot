353a84c4505723a7c34d4bab93e7f0713fea409f
SvenBrnn <svenbrnn@web.de>
2024-04-06 12:54:46 +0200

    Update zKillSubscriber.ts

--- src/src/zKillSubscriber.ts
+++ src/src/zKillSubscriber.ts
@@ -94,7 +94,7 @@ export class ZKillSubscriber {
                     host: 'redis',
                     port: 6379,
                 },
-                concurrency: 10
+                concurrency: 10,
             });
 
             // Initialize the queue
